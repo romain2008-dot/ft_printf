cc41f214741b0cd99fc14afbe0a1a1c0ac870e05
Romain Gutierrez <gutierrez08rom@gmail.com>
2025-12-08 01:17:15 +0100

    ah il est vite fait ton commit l'ancien

---
+++ ft_printf.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romgutie <romgutie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/04 15:33:38 by romgutie          #+#    #+#             */
+/*   Updated: 2025/12/08 01:15:37 by romgutie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		i;
+	int		count;
+
+	va_start(args, format);
+	i = 0;
+	count = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			count += ft_dispatch(format[i], args);
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
+
+int	ft_dispatch(char specifier, va_list args)
+{
+	if (specifier == 'c')
+		return (ft_putchar(va_arg(args, int)));
+	else if (specifier == 's')
+		return (ft_putstr(va_arg(args, char *)));
+	else if (specifier == 'p')
+		return ();
+	else if (specifier == 'd' || specifier == 'i')
+		return ();
+	else if (specifier == 'u')
+		return ();
+	else if (specifier == 'x')
+		return ();
+	else if (specifier == 'X')
+		return ();
+	else if (specifier == '%')
+		return ();
+	return (0);
+}
